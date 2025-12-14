709c02479146cd6a77d13ab9b6c42979666ba87b
AbdoulayeAbdoulaye Alhassane Diallo <alhassaneabdoulaye@gmail.com>
2025-12-14 11:24:31 -0500

    add basic implementation of linked list class and invoke

--- datastructure/scottDS/linkedList.py
+++ datastructure/scottDS/linkedList.py
@@ -6,8 +6,14 @@ class Node:
 
 # create the class list 
 class List:
+  # list list constructor 
   def __init__(self, value):
     New_Node = Node(value)
     self.head = New_Node
     self.tail = New_Node
-    
+    self.length = 1
+
+# invoking the linked List class 
+my_list_List = List(5)
+
+print(my_list_List.length)
